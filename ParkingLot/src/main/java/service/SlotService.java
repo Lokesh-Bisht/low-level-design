73de47e4705037c605b8ab358a05ea1f391c91ea
Lokesh Bisht <lokeshbisht.096@gmail.com>
2023-10-17 00:07:04 +0530

    feat: 30 Free parking slot

--- ParkingLot/src/main/java/service/SlotService.java
+++ ParkingLot/src/main/java/service/SlotService.java
@@ -2,9 +2,11 @@ package service;
 
 import enums.VehicleType;
 import exceptions.InvalidVehicleException;
+import models.Ticket;
 
 public interface SlotService {
 
     boolean hasParkingSlots(VehicleType vehicleType) throws InvalidVehicleException;
     int reserveParkingSlot(VehicleType vehicleType);
+    void freeParkingSlot(Ticket ticket);
 }
