8140bc61aa217dc6cbf536a83d3c77219c1c41f1
Lokesh Bisht <lokeshbisht.096@gmail.com>
2023-12-19 23:50:32 +0530

    Add course models and constants

---
+++ Course-Scheduling/src/main/java/com/example/geektrust/models/CourseRegistration.java
@@ -0,0 +1,57 @@
+/**
+ * Author: Lokesh Bisht
+ */
+
+package com.example.geektrust.models;
+
+import com.example.geektrust.enums.CourseStatus;
+
+public class CourseRegistration {
+
+    private String courseRegistrationId;
+
+    private String employeeEmail;
+
+    private String courseOfferingId;
+
+    private CourseStatus status;
+
+    public CourseRegistration(String courseRegistrationId, String employeeEmail, String courseOfferingId, CourseStatus status) {
+        this.courseRegistrationId = courseRegistrationId;
+        this.employeeEmail = employeeEmail;
+        this.courseOfferingId = courseOfferingId;
+        this.status = status;
+    }
+
+    public String getCourseRegistrationId() {
+        return courseRegistrationId;
+    }
+
+    public void setCourseRegistrationId(String courseRegistrationId) {
+        this.courseRegistrationId = courseRegistrationId;
+    }
+
+    public String getEmployeeEmail() {
+        return employeeEmail;
+    }
+
+    public void setEmployeeEmail(String employeeEmail) {
+        this.employeeEmail = employeeEmail;
+    }
+
+    public String getCourseOfferingId() {
+        return courseOfferingId;
+    }
+
+    public void setCourseOfferingId(String courseOfferingId) {
+        this.courseOfferingId = courseOfferingId;
+    }
+
+    public CourseStatus getStatus() {
+        return status;
+    }
+
+    public void setStatus(CourseStatus status) {
+        this.status = status;
+    }
+}
